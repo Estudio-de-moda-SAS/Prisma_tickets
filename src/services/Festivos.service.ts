ed87d867b56476df3a7a53a6f04ec51c7a44b6d9
Daniel Palacios Vivero <dpalacios@estudiodemoda.com.co>
2026-04-10 11:13:45 -0500

    Reinicio del repositio

--- src/services/Festivos.service.ts
+++
@@ -1,7 +0,0 @@
-import holidaysColombia, { type Holiday } from "festivos-colombianos";
-
-export async function fetchHolidays(): Promise<Holiday[]> {
-  const year = new Date().getFullYear(); //Obtener el año actual
-  const holidays = holidaysColombia(year);
-  return holidays
-}
